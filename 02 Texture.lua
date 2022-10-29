af90610f120c15f2ed6d69c4d954223ec49b3127
02Dcs <95237577+02Dcs@users.noreply.github.com>
2022-10-29 14:48:13 +0200

    Update 02 Texture.lua

--- 02 Texture.lua
+++ 02 Texture.lua
@@ -1,22 +1,18 @@
-getgenv().PinkMode =  true;
-
 local sky = Instance.new"Sky" 
 for z, K in pairs(game.Lighting:GetChildren()) do 
-	if K:IsA"Sky"then 
-		K:Remove(); delay(.25)
-	end
-	return nil
+if K:IsA"Sky"then 
+K:Remove(); delay(.25)
+end
+return nil
 end
 
 if game.Lighting ~= nil then 
-	if getgenv().PinkMode == true then 
-	sky.Name = "Texture"
-	sky.SkyboxBk = "http://www.roblox.com/asset/?id=271042516"
-	sky.SkyboxDn = "http://www.roblox.com/asset/?id=271077243"
-	sky.SkyboxFt = "http://www.roblox.com/asset/?id=271042556"
-	sky.SkyboxLf = "http://www.roblox.com/asset/?id=271042310"
-	sky.SkyboxRt = "http://www.roblox.com/asset/?id=271042467"
-	sky.SkyboxUp = "http://www.roblox.com/asset/?id=271077958"
-	sky.Parent = game.Lighting
-     end
+sky.Name = "Texture"
+sky.SkyboxBk = "http://www.roblox.com/asset/?id=271042516"
+sky.SkyboxDn = "http://www.roblox.com/asset/?id=271077243"
+sky.SkyboxFt = "http://www.roblox.com/asset/?id=271042556"
+sky.SkyboxLf = "http://www.roblox.com/asset/?id=271042310"
+sky.SkyboxRt = "http://www.roblox.com/asset/?id=271042467"
+sky.SkyboxUp = "http://www.roblox.com/asset/?id=271077958"
+sky.Parent = game.Lighting
 end
