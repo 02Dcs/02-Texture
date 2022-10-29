e1b1668407ad02cfa4cc7d4e49b4f1cb5c662329
02Dcs <95237577+02Dcs@users.noreply.github.com>
2022-10-29 15:15:27 +0200

    Update texture.lua

--- texture.lua
+++ texture.lua
@@ -6,13 +6,15 @@ sky.SkyboxFt="http://www.roblox.com/asset/?id=150939047"
 sky.SkyboxLf="http://www.roblox.com/asset/?id=150939056"
 sky.SkyboxRt="http://www.roblox.com/asset/?id=150939063"
 sky.SkyboxUp="http://www.roblox.com/asset/?id=150939082"
-sky.Paren =game.Lighting
+sky.Parent=game.Lighting
 
 local m=0 
 for z, K in pairs(game.Lighting:GetChildren()) do 
 if K:IsA"Sky" and not K.Name =="Texture" then 
 K:Remove(); delay(.25)
 m = m + 1
+else 
+print"Texture V1.0"
 end
 return nil
 end
