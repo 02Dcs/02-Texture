b586303fbef5b78bc5fc632bf37df89a737329ee
02Dcs <95237577+02Dcs@users.noreply.github.com>
2022-10-29 14:55:42 +0200

    Update 02 Texture.lua

--- 02 Texture.lua
+++ 02 Texture.lua
@@ -3,16 +3,16 @@ for z, K in pairs(game.Lighting:GetChildren()) do
 if K:IsA"Sky"then 
 K:Remove(); delay(.25)
 end
-return nil
-end
-
 if game.Lighting ~= nil then 
 sky.Name = "Texture"
 sky.SkyboxBk = "http://www.roblox.com/asset/?id=150939022"
 sky.SkyboxDn = "http://www.roblox.com/asset/?id=150939038"
 sky.SkyboxFt = "http://www.roblox.com/asset/?id=150939047"
-sky.SkyboxLf = "http://www.roblox.com/asset/?id=271042310"
+sky.SkyboxLf = "http://www.roblox.com/asset/?id=150939056"
 sky.SkyboxRt = "http://www.roblox.com/asset/?id=150939063"
 sky.SkyboxUp = "http://www.roblox.com/asset/?id=150939082"
 sky.Parent = game.Lighting
 end
+return nil
+end
+
