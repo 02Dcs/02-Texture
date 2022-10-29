fb8c8e18cf4dc2f3f3621e7c598f83c854c8d4e2
02Dcs <95237577+02Dcs@users.noreply.github.com>
2022-10-29 15:14:16 +0200

    Update texture.lua

--- texture.lua
+++ texture.lua
@@ -1,14 +1,14 @@
-local sky = Instance.new"Sky" 
-sky.Name = "Texture"
-sky.SkyboxBk = "http://www.roblox.com/asset/?id=150939022"
-sky.SkyboxDn = "http://www.roblox.com/asset/?id=150939038"
-sky.SkyboxFt = "http://www.roblox.com/asset/?id=150939047"
-sky.SkyboxLf = "http://www.roblox.com/asset/?id=150939056"
-sky.SkyboxRt = "http://www.roblox.com/asset/?id=150939063"
-sky.SkyboxUp = "http://www.roblox.com/asset/?id=150939082"
-sky.Parent = game.Lighting
+local sky=Instance.new"Sky" 
+sky.Name="Texture"
+sky.SkyboxBk="http://www.roblox.com/asset/?id=150939022"
+sky.SkyboxDn="http://www.roblox.com/asset/?id=150939038"
+sky.SkyboxFt="http://www.roblox.com/asset/?id=150939047"
+sky.SkyboxLf="http://www.roblox.com/asset/?id=150939056"
+sky.SkyboxRt="http://www.roblox.com/asset/?id=150939063"
+sky.SkyboxUp="http://www.roblox.com/asset/?id=150939082"
+sky.Paren =game.Lighting
 
-local m = 0 
+local m=0 
 for z, K in pairs(game.Lighting:GetChildren()) do 
 if K:IsA"Sky" and not K.Name =="Texture" then 
 K:Remove(); delay(.25)
