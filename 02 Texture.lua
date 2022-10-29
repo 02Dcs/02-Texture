8fdaeb2a3517bcc4b4ec52613aaeba921713a52d
02Dcs <95237577+02Dcs@users.noreply.github.com>
2022-10-29 14:40:42 +0200

    Update and rename 02 Texture to 02 Texture.lua

---
+++ 02 Texture.lua
@@ -0,0 +1,18 @@
+local sky = Instance.new"Sky" 
+for z, K in pairs(game.Lighting:GetChildren()) do 
+	if K:IsA"Sky"then 
+		K:Remove(); delay(.25)
+	end
+	return nil
+end
+
+if game.Lighting ~= nil then 
+	sky.Name = "Texture"
+	sky.SkyboxBk = "http://www.roblox.com/asset/?id=150939022"
+	sky.SkyboxDn = "http://www.roblox.com/asset/?id=150939038"
+	sky.SkyboxFt = "http://www.roblox.com/asset/?id=150939047"
+	sky.SkyboxLf = "http://www.roblox.com/asset/?id=271042310"
+	sky.SkyboxRt = "http://www.roblox.com/asset/?id=15093906"
+	sky.SkyboxUp = "http://www.roblox.com/asset/?id=150939082"
+	sky.Parent = game.Lighting
+end
